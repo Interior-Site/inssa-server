0524d384321aad314470f3d3612ecd54a0abd980
NaEunHye <eunhye_3@naver.com>
2023-01-14 14:53:33 +0900

    swagger ui 적용 및 spring boot 버전 변경

---
+++ src/main/java/com/inssa/api/controller/TestController.java
@@ -0,0 +1,36 @@
+package com.inssa.api.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+@RequiredArgsConstructor
+@Tag(name = "test", description = "Test API")
+public class TestController {
+
+	@Tag(name = "test") // API 그룹의 이름 지정
+	@Operation(summary = "test hello", description = "hello api example") // API 별 설명 추가
+	@ApiResponses({ // 반환 타입 설정
+		@ApiResponse(responseCode = "200", description = "OK !!"),
+		@ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+		@ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+		@ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+	})
+	@GetMapping("/hello")
+	public ResponseEntity<String> hello (
+		@Parameter(description = "이름", required = true, example = "Park")
+		@RequestParam
+		String name) {
+		return ResponseEntity.ok("hello " + name);
+	}
+}
