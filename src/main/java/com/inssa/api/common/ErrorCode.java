5c4ed7e660811f67e80cf6f9d4672d894fe4f589
ddkzzang <84914729+ddkzzang@users.noreply.github.com>
2023-01-25 22:47:41 +0900

    2023.01.25 Commit test

---
+++ src/main/java/com/inssa/api/common/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.inssa.api.common;
+
+import lombok.Data;
+
+@Data
+public class ErrorCode {
+	
+	/**
+	 * 테스트용 에러코드
+	 */
+	public static final int DEFAULT = 600;
+
+}
--- src/main/java/com/inssa/api/common/StatusCode.java
+++ src/main/java/com/inssa/api/common/StatusCode.java
@@ -1,5 +1,18 @@
 package com.inssa.api.common;
 
+import lombok.Data;
+
+@Data
 public class StatusCode {
+	
+	/**
+	 * 로그인 성공코드
+	 */
+	public static final int LOGIN_SUCCESS = 200;
+	
+	/**
+	 * 로그인 실패코드
+	 */
+	public static final int LOGIN_FAIL = 400;
 
 }
