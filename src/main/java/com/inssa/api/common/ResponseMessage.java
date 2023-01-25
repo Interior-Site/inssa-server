c36b7e2ba2456b885e2e3daf0090f03d782cbf21
ddkzzang <84914729+ddkzzang@users.noreply.github.com>
2023-01-25 22:47:41 +0900

    2023.01.25 Commit test

--- src/main/java/com/inssa/api/common/ResponseMessage.java
+++ src/main/java/com/inssa/api/common/ResponseMessage.java
@@ -1,9 +1,18 @@
 package com.inssa.api.common;
 
+import lombok.Data;
+
+@Data
 public class ResponseMessage {
 	
+	/**
+	 * 로그인 성공 코드
+	 */
 	public static final String LOGIN_SUCCESS = "로그인 성공하였습니다.";
 	
+	/**
+	 * 로그인 실패 코드
+	 */
 	public static final String LOGIN_FAIL = "로그인에 실패하였습니다.";
 
 }
