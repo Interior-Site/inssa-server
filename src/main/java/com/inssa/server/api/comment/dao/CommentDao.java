517913c895eea25e3f83b128338878fb630c3a35
df0404 <df0404@hamnail.net>
2023-02-05 23:47:29 +0900

    2023/02/05 어노테이션 추가

--- src/main/java/com/inssa/server/api/comment/dao/CommentDao.java
+++ src/main/java/com/inssa/server/api/comment/dao/CommentDao.java
@@ -1,4 +1,9 @@
 package com.inssa.server.api.comment.dao;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
 public class CommentDao {
 }
--- src/main/java/com/inssa/server/api/comment/service/CommentService.java
+++ src/main/java/com/inssa/server/api/comment/service/CommentService.java
@@ -1,4 +1,9 @@
 package com.inssa.server.api.comment.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service("CommentService")
+@RequiredArgsConstructor
 public class CommentService {
 }
