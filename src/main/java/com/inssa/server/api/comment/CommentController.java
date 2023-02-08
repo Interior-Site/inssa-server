7fcbc05a600a6f6850969b9331061d106361c86f
df0404 <df0404@hamnail.net>
2023-02-09 00:01:47 +0900

    2023/02/08 DTO 수정

--- src/main/java/com/inssa/server/api/comment/CommentController.java
+++ src/main/java/com/inssa/server/api/comment/CommentController.java
@@ -1,4 +1,12 @@
 package com.inssa.server.api.comment;
 
+import com.inssa.server.api.comment.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor //
 public class CommentController {
+
+    private CommentService commentService;
 }
---
+++ src/main/java/com/inssa/server/api/comment/dto/CommentDto.java
@@ -0,0 +1,23 @@
+package com.inssa.server.api.comment.dto;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+
+@Repository
+@Data
+public class CommentDto {
+
+    private int commentNo; // 댓글번호
+    private String commentContent; // 댓글내용
+    private String commentStatus; // 댓글상태
+    private Timestamp commentRegDate; // 댓글 등록일
+    private String commentImg; // 첨부파일
+    private int boardNo; // 게시글번호
+    private int userNo; // 작성자번호
+
+
+
+}
