8bc8e5d5b7a2493b3fad86470dd8b7ac2cf340d1
Aram <df0404@hanmail.net>
2023-02-13 23:13:07 +0900

    fix: 댓글 기능 수정중

--- src/main/java/com/inssa/server/api/comment/CommentController.java
+++ src/main/java/com/inssa/server/api/comment/CommentController.java
@@ -1,10 +1,14 @@
 package com.inssa.server.api.comment;
 
 import com.inssa.server.api.comment.service.CommentService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/comment")
+@Tag(name = "user", description = "댓글 API") // 테스트 할 때
 @RequiredArgsConstructor //
 public class CommentController {
 
