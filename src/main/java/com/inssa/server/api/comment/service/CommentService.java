23555f5caeee86e21e770872d95ec9ccde7bc8c7
Aram <df0404@hanmail.net>
2023-02-14 23:25:34 +0900

    fix: 댓글 목록 조회 수정중

--- src/main/java/com/inssa/server/api/comment/service/CommentService.java
+++ src/main/java/com/inssa/server/api/comment/service/CommentService.java
@@ -1,9 +1,22 @@
 package com.inssa.server.api.comment.service;
 
+import com.inssa.server.api.comment.dao.CommentDao;
+import com.inssa.server.api.comment.dto.CommentDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("CommentService")
 @RequiredArgsConstructor
 public class CommentService {
+
+    @Autowired
+    private final CommentDao commentdao;
+
+    public List<CommentDto> selectList(int commentNo){
+        return commentdao.selectList(commentNo);
+    }
+
 }
