9970cbeb1cb79451958999a111f0ed432544b4b0
Aram <df0404@hanmail.net>
2023-02-15 23:22:07 +0900

    fix: 댓글 수정

--- src/main/java/com/inssa/server/api/comment/mapper/CommentMapper.java
+++ src/main/java/com/inssa/server/api/comment/mapper/CommentMapper.java
@@ -3,7 +3,9 @@ package com.inssa.server.api.comment.mapper;
 import com.inssa.server.api.comment.dto.CommentDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface CommentMapper {
-    CommentDto selectList(int commentNo);
+    List<CommentDto> selectList(int commentNo);
 }
