987bce384dc40a56ace753c7572837fad7297556
eunhye3333 <eunhye_3@naver.com>
2023-07-19 16:24:20 +0900

    feat : 공통 api response 추가

---
+++ src/main/java/com/inssa/server/common/response/InssaApiResponse.java
@@ -0,0 +1,24 @@
+package com.inssa.server.common.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "API 요청 결과")
+public class InssaApiResponse {
+    private String code;
+    private String message;
+    private Object result;
+
+    public InssaApiResponse(String code, String message, Object result) {
+        this.code = code;
+        this.message = message;
+        this.result = result;
+    }
+
+    public static InssaApiResponse ok(Object result) {
+        return new InssaApiResponse("OK", "API 요청 성공", result);
+    }
+
+    public static InssaApiResponse ok() {
+        return new InssaApiResponse("OK", "API 요청 성공", null);
+    }
+}
