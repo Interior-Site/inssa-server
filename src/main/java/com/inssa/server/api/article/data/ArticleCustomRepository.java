fb28ac6b63a195d37cb27b35be8817ace8d164c7
eunhye3333 <eunhye_3@naver.com>
2023-07-31 17:47:46 +0900

    feat : 게시판 목록 조회 api 생성

--- src/main/java/com/inssa/server/api/article/data/ArticleCustomRepository.java
+++ src/main/java/com/inssa/server/api/article/data/ArticleCustomRepository.java
@@ -1,11 +1,10 @@
 package com.inssa.server.api.article.data;
 
 import com.inssa.server.api.article.dto.ArticleListResponseDto;
-import com.inssa.server.api.article.model.Article;
 import com.inssa.server.api.article.model.ArticleType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<Article, Long>, ArticleCustomRepository{
+public interface ArticleCustomRepository {
+    Page<ArticleListResponseDto> findArticles(ArticleType type, Pageable pageable);
 }
