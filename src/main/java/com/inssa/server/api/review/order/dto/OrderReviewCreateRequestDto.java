8608680ca6fc4b6be0253304775ee4be18435681
Dahee Goo <dadaahee@gmail.com>
2023-08-02 07:37:49 +0900

    feat: 견적 후기 컨트롤러, 서비스, 레포지토리 추가

---
+++ src/main/java/com/inssa/server/api/review/order/dto/OrderReviewCreateRequestDto.java
@@ -0,0 +1,8 @@
+package com.inssa.server.api.review.order.dto;
+
+public class OrderReviewCreateRequestDto {
+
+    public OrderReviewRequestDto toDto() {
+        return null;
+    }
+}
---
+++ src/main/java/com/inssa/server/api/review/order/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.inssa.server.api.review.order.service;
+
+import com.inssa.server.api.review.build.dto.BuildDto;
+import com.inssa.server.api.review.order.data.OrderReviewRepository;
+import com.inssa.server.api.review.order.dto.OrderReviewRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service("OrderService")
+public class OrderService {
+
+    private final OrderReviewRepository orderReviewRepository;
+
+    public Object createOrderReview(OrderReviewRequestDto request) {
+        return null;
+    }
+}
