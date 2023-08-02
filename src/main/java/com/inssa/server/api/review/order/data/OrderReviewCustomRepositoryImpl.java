b830d5a62a07fcdc8934a52d152151d8a6f19ddb
Dahee Goo <dadaahee@gmail.com>
2023-08-03 01:06:22 +0900

    feat: 견적후기 API - CRUD 껍데기 작성

--- src/main/java/com/inssa/server/api/review/order/data/OrderReviewCustomRepositoryImpl.java
+++ src/main/java/com/inssa/server/api/review/order/data/OrderReviewCustomRepositoryImpl.java
@@ -1,10 +1,18 @@
 package com.inssa.server.api.review.order.data;
 
+import com.inssa.server.api.review.order.dto.OrderReviewListResponseDto;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @RequiredArgsConstructor
 public class OrderReviewCustomRepositoryImpl implements OrderReviewCustomRepository {
-
     private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Page<OrderReviewListResponseDto> findOrderReviews(Pageable pageable) {
+        // TODO
+        return null;
+    }
 }
--- src/main/java/com/inssa/server/api/review/order/service/OrderService.java
+++ src/main/java/com/inssa/server/api/review/order/service/OrderService.java
@@ -1,8 +1,14 @@
 package com.inssa.server.api.review.order.service;
 
 import com.inssa.server.api.review.order.data.OrderReviewRepository;
+import com.inssa.server.api.review.order.dto.OrderReviewListResponseDto;
 import com.inssa.server.api.review.order.dto.OrderReviewRequestDto;
+import com.inssa.server.api.review.order.dto.OrderReviewResponseDto;
+import com.inssa.server.api.review.order.model.OrderReview;
+import com.inssa.server.common.exception.InssaException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -11,7 +17,17 @@ public class OrderService {
 
     private final OrderReviewRepository orderReviewRepository;
 
-    public Object createOrderReview(OrderReviewRequestDto request) {
-        return null;
+    public Long createOrderReview(OrderReviewRequestDto request) {
+        OrderReview orderReview = orderReviewRepository.save(request.toEntity());
+        return orderReview.getNo();
+    }
+
+    public Page<OrderReviewListResponseDto> findOrderReviews(Pageable pageable) {
+        return orderReviewRepository.findOrderReviews(pageable);
+    }
+
+    public OrderReviewResponseDto findById(Long orderReviewNo) {
+        return new OrderReviewResponseDto(orderReviewRepository.findById(orderReviewNo)
+                .orElseThrow(() -> new InssaException("견적 후기가 존재하지 않습니다.")));
     }
 }
