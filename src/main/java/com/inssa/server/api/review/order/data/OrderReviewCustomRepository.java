ac17c50827c97c4bc38d08dabb71f662c351254f
Dahee Goo <dadaahee@gmail.com>
2023-08-04 03:52:18 +0900

    chore: QueryDSL 코드 임시 주석 처리

--- src/main/java/com/inssa/server/api/review/order/data/OrderReviewCustomRepository.java
+++ src/main/java/com/inssa/server/api/review/order/data/OrderReviewCustomRepository.java
@@ -1,9 +1,10 @@
 package com.inssa.server.api.review.order.data;
 
-import com.inssa.server.api.review.order.dto.OrderReviewListResponseDto;
+//import com.inssa.server.api.review.order.dto.OrderReviewListResponseDto;
+import com.inssa.server.api.review.order.model.OrderReview;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface OrderReviewCustomRepository {
-    Page<OrderReviewListResponseDto> findOrderReviews(Pageable pageable);
+//    Page<OrderReviewListResponseDto> findOrderReviews(Pageable pageable);
 }
