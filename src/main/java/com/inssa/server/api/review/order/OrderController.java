6aa0d49d148a0032a5029848cc6206342ae1ed2b
Dahee Goo <dadaahee@gmail.com>
2023-08-03 22:41:18 +0900

    feat: 로그인 여부 검증 추가

--- src/main/java/com/inssa/server/api/review/order/OrderController.java
+++ src/main/java/com/inssa/server/api/review/order/OrderController.java
@@ -2,8 +2,8 @@ package com.inssa.server.api.review.order;
 
 import com.inssa.server.api.review.order.dto.*;
 import com.inssa.server.api.review.order.service.OrderService;
+import com.inssa.server.api.user.annotation.LoggedInAndActive;
 import com.inssa.server.api.user.model.AuthUser;
-import com.inssa.server.common.exception.InssaException;
 import com.inssa.server.common.response.InssaApiResponse;
 import com.inssa.server.common.response.ResponseCode;
 import io.swagger.v3.oas.annotations.Operation;
@@ -29,14 +29,11 @@ public class OrderController {
 
     @Operation(summary = "견적 후기 등록", tags = "orderReview")
     @PostMapping
+    @LoggedInAndActive
     public InssaApiResponse<Map<String, Object>> createOrderReview(
             @RequestBody OrderReviewCreateRequestDto createRequest,
             @AuthenticationPrincipal AuthUser user
     ) {
-        // user 판단
-        if (user == null) {
-            throw new InssaException("로그인 후 이용 가능합니다.");
-        }
         OrderReviewRequestDto requestDto = createRequest.toDto();
         Long orderReviewNo = orderService.createOrderReview(requestDto);
         // [header] Location: /api/v1/review/orders/{orderReviewNo}
