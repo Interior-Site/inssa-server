1d2989bcf7ff6dd6e93537bd51e5a809551706ee
eunhye3333 <eunhye_3@naver.com>
2023-08-03 17:31:47 +0900

    fix : ROLE_ prefix 추가

--- src/main/java/com/inssa/server/common/annotation/PreAuthorizeLogInUser.java
+++ src/main/java/com/inssa/server/common/annotation/PreAuthorizeLogInUser.java
@@ -7,8 +7,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasRole('USER')")
+@PreAuthorize("hasRole('ROLE_USER')")
 public @interface PreAuthorizeLogInUser {
 }
