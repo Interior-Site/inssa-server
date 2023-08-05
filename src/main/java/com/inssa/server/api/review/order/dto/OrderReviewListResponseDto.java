2b5a82966ff9663c447ef65e37d31a217967e14d
Dahee Goo <dadaahee@gmail.com>
2023-08-06 02:03:49 +0900

    feat: 견적 후기 DTO 작성, 일부 linter 적용

--- src/main/java/com/inssa/server/api/review/order/dto/OrderReviewListResponseDto.java
+++ src/main/java/com/inssa/server/api/review/order/dto/OrderReviewListResponseDto.java
@@ -1,23 +1,49 @@
 package com.inssa.server.api.review.order.dto;
 
+import com.inssa.server.api.review.build_type.dto.BuildTypeListResponse;
+import com.inssa.server.api.review.category.dto.CategoryListResponse;
 import com.inssa.server.api.review.order.model.OrderReview;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 
+@RequiredArgsConstructor
+@Getter
 public class OrderReviewListResponseDto {
 
+    /**
+     * 제목
+     */
     private final String title;
+
+    /**
+     * 내용
+     */
     private final String content;
+
+    /**
+     * 견적 금액
+     */
     private final int amount;
 
-//    private final List<CategoryListResponse> categories;
-//    private final List<BuildTypeListResponse> buildTypes;
+    /**
+     * 건물 유형
+     */
+    private final List<CategoryListResponse> categories;
+
+    /**
+     * 시공 유형
+     */
+    private final List<BuildTypeListResponse> buildTypes;
 
     public OrderReviewListResponseDto(OrderReview orderReview){
         this.title = orderReview.getTitle();
         this.content = orderReview.getContent();
         this.amount = orderReview.getAmount();
-//        this.categories = null;
-//        this.buildTypes = null;
+        this.categories = null;
+        this.buildTypes = null;
     }
 
 }
--- src/main/java/com/inssa/server/api/review/order/dto/OrderReviewResponseDto.java
+++ src/main/java/com/inssa/server/api/review/order/dto/OrderReviewResponseDto.java
@@ -1,12 +1,41 @@
 package com.inssa.server.api.review.order.dto;
 
+import com.inssa.server.api.company.dto.CompanyResponseDto;
+import com.inssa.server.api.company.model.Company;
+import com.inssa.server.api.review.build_type.model.BuildType;
+import com.inssa.server.api.review.category.model.Category;
 import com.inssa.server.api.review.order.model.OrderReview;
+import com.inssa.server.share.board.BoardStatus;
+import lombok.Getter;
 
+import java.util.List;
+
+@Getter
 public class OrderReviewResponseDto {
 
-    // TODO
+    private Long no;
+    private Long userNo;
+    private int amount;
+    private String title;
+    private String content;
+    private BoardStatus status;
+    private int viewCount;
 
-    public OrderReviewResponseDto(OrderReview orderReview) {
-        // TODO
+    private CompanyResponseDto company;
+
+    private List<BuildTypeResponseDto> buildTypes;
+    private List<CategoryResponseDto> categories;
+
+    public OrderReviewResponseDto(OrderReview orderReview, Company company, List<BuildType> buildTypes, List<Category> categories) {
+        this.no = orderReview.getNo();
+        this.userNo = orderReview.getUserNo();
+        this.amount = orderReview.getAmount();
+        this.title = orderReview.getTitle();
+        this.content = orderReview.getContent();
+        this.status = orderReview.getStatus();
+        this.viewCount = orderReview.getViewCount();
+        this.company = new CompanyResponseDto(company);
+        this.buildTypes = buildTypes.stream().map(BuildTypeResponseDto::new).toList();
+        this.categories = categories.stream().map(CategoryResponseDto::new).toList();
     }
 }
