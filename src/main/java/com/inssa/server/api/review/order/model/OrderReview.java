af52318e96b79285d6c9b02759bed4c3c815c8a7
Dahee Goo <dadaahee@gmail.com>
2023-08-06 01:55:53 +0900

    refactor: CRUD 코드 스타일 통일, 빌더 및 메서드 추가

--- src/main/java/com/inssa/server/api/review/order/model/OrderReview.java
+++ src/main/java/com/inssa/server/api/review/order/model/OrderReview.java
@@ -1,7 +1,11 @@
 package com.inssa.server.api.review.order.model;
 
 import com.inssa.server.api.company.model.Company;
+import com.inssa.server.api.review.build_type.model.BuildType;
+import com.inssa.server.api.review.category.model.Category;
 import com.inssa.server.api.user.model.User;
+import com.inssa.server.common.code.ErrorCode;
+import com.inssa.server.common.exception.InssaException;
 import com.inssa.server.share.board.BoardStatus;
 import com.inssa.server.share.entity.BaseTimeEntity;
 import jakarta.persistence.*;
@@ -12,6 +16,7 @@ import org.hibernate.annotations.ColumnDefault;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @NoArgsConstructor
@@ -60,12 +65,98 @@ public class OrderReview extends BaseTimeEntity {
     private List<OrderReviewCategory> orderReviewCategories = new ArrayList<>();
 
     @Builder
-    public OrderReview(int amount, String title, String content, BoardStatus status, Long userNo, Long companyNo) {
+    public OrderReview(int amount, String title, String content, Long userNo, Long companyNo) {
         this.amount = amount;
         this.title = title;
         this.content = content;
-        this.status = status;
         this.userNo = userNo;
         this.companyNo = companyNo;
+    }
+
+    public void addReviewCategory(OrderReviewCategory category) {
+        this.getOrderReviewCategories().add(category);
+        category.setOrderReview(this);
+    }
+
+    public void addReviewBuildType(OrderReviewBuildType buildType) {
+        this.getOrderReviewBuildTypes().add(buildType);
+        buildType.setOrderReview(this);
+    }
+
+    public void updateReviewCategory(List<OrderReviewCategory> categories) {
+        if (Objects.nonNull(this.orderReviewCategories)) {
+            this.getOrderReviewCategories().clear();
+        }
+        categories.forEach(this::addReviewCategory);
+    }
+
+    public void updaddReviewBuildType(List<OrderReviewBuildType> buildTypes) {
+        if (Objects.nonNull(this.orderReviewCategories)) {
+            this.getOrderReviewBuildTypes().clear();
+        }
+        buildTypes.forEach(this::addReviewBuildType);
+    }
+
+    public void updateCompany(Company company) {
+        if (!Objects.equals(company.getNo(), this.companyNo)){
+            this.company = company;
+        }
+    }
+
+    public OrderReview updateOrderReview(
+            int amount,
+            String title,
+            String content,
+            Company company,
+            List<OrderReviewBuildType> buildTypes,
+            List<OrderReviewCategory> categories
+    ) {
+        if (!Objects.equals(this.amount, amount)) {
+            this.amount = amount;
+        }
+        if (!Objects.equals(this.title, title) && Objects.nonNull(title) && !title.isBlank()) {
+            this.title = title;
+        }
+        if (!Objects.equals(this.content, content) && Objects.nonNull(content) && !content.isBlank()) {
+            this.content = content;
+        }
+        if (!Objects.equals(company.getNo(), this.companyNo)){
+            this.company = company;
+        }
+        if (!this.orderReviewBuildTypes.isEmpty()) {
+            this.orderReviewBuildTypes.clear();
+        }
+        if (!this.orderReviewCategories.isEmpty()) {
+            this.orderReviewCategories.clear();
+        }
+        if (!buildTypes.isEmpty()){
+            this.orderReviewBuildTypes = buildTypes;
+        }
+        if (!categories.isEmpty()) {
+            this.orderReviewCategories = categories;
+        }
+        return this;
+    }
+
+    public void increaseViewCount() {
+        this.viewCount ++;
+    }
+
+    public List<BuildType> getBuildTypes() {
+        if (!this.orderReviewBuildTypes.isEmpty()) {
+            return orderReviewBuildTypes.stream()
+                    .map(OrderReviewBuildType::getBuildType)
+                    .toList();
+        }
+        throw new InssaException(ErrorCode.INVALID, "건물 유형 정보가 올바르지 않습니다.");
+    }
+
+    public List<Category> getCategories() {
+        if (!this.orderReviewCategories.isEmpty()){
+            return orderReviewCategories.stream()
+                    .map(OrderReviewCategory::getCategory)
+                    .toList();
+        }
+        throw new InssaException(ErrorCode.INVALID, "시공 유형 정보가 올바르지 않습니다.");
     }
 }
--- src/main/java/com/inssa/server/api/review/order/model/OrderReviewBuildType.java
+++ src/main/java/com/inssa/server/api/review/order/model/OrderReviewBuildType.java
@@ -3,8 +3,10 @@ package com.inssa.server.api.review.order.model;
 import com.inssa.server.api.review.build_type.model.BuildType;
 import com.inssa.server.share.entity.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
@@ -16,11 +18,21 @@ public class OrderReviewBuildType extends BaseTimeEntity {
     @Column(name = "order_build_no")
     private Long no;
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_no", insertable = false, updatable = false, nullable = false)
+    @JoinColumn(name = "orderReviewNo", insertable = false, updatable = false, nullable = false)
     private OrderReview orderReview;
+    private Long orderReviewNo;
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "build_type_no", insertable = false, updatable = false, nullable = false)
+    @JoinColumn(name = "BuildTypeNo", insertable = false, updatable = false, nullable = false)
     private BuildType buildType;
+    private Long buildTypeNo;
+
+    @Builder
+    public OrderReviewBuildType(OrderReview orderReview, BuildType buildType) {
+        this.orderReview = orderReview;
+        this.buildType = buildType;
+    }
 }
