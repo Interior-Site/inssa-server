f667b342e5de9645f61be81156cc0c74ed92805e
Dahee Goo <dadaahee@gmail.com>
2023-08-06 01:55:53 +0900

    refactor: CRUD 코드 스타일 통일, 빌더 및 메서드 추가

--- src/main/java/com/inssa/server/api/review/order/model/OrderReviewCategory.java
+++ src/main/java/com/inssa/server/api/review/order/model/OrderReviewCategory.java
@@ -3,8 +3,10 @@ package com.inssa.server.api.review.order.model;
 import com.inssa.server.api.review.category.model.Category;
 import com.inssa.server.share.entity.BaseTimeEntity;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
@@ -16,11 +18,21 @@ public class OrderReviewCategory extends BaseTimeEntity {
     @Column(name = "order_category_no")
     private Long no;
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_no", insertable = false, updatable = false, nullable = false)
+    @JoinColumn(name = "orderReviewNo", insertable = false, updatable = false, nullable = false)
     private OrderReview orderReview;
+    private Long orderReviewNo;
 
+    @Setter
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_no", insertable = false, updatable = false, nullable = false)
+    @JoinColumn(name = "categoryNo", insertable = false, updatable = false, nullable = false)
     private Category category;
+    private Long categoryNo;
+
+    @Builder
+    public OrderReviewCategory(OrderReview orderReview, Category category) {
+        this.orderReview = orderReview;
+        this.category = category;
+    }
 }
