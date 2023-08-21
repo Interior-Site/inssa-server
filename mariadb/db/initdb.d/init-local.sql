661a7912458e08bc1494ef5491008cfa5322c252
Dahee Goo <dadaahee@gmail.com>
2023-08-21 17:54:34 +0900

    chore: test dummy data 수정

--- mariadb/db/initdb.d/init-local.sql
+++ mariadb/db/initdb.d/init-local.sql
@@ -15,9 +15,9 @@ INSERT INTO inssa.category (NAME) VALUES ('벽지');
 
 INSERT INTO INSSA.user (created_date, modified_date, profile_no, quit_date, email, nickname, password, role, status) VALUES ('2023-08-08 17:10:00.971513', '2023-08-08 17:10:00.971513', null, null, 'test@gmail.com', 'test', '$2a$10$ggkeXY7KA3LwcoACzSF2OO/ev.A4HwWaTMeniLczQ/v.AZFrQWq3W', 'USER', 'ACTIVATED');
 INSERT INTO INSSA.user (created_date, modified_date, profile_no, quit_date, email, nickname, password, role, status) VALUES ('2023-08-08 17:20:43.961376', '2023-08-08 17:20:43.961376', null, null, 'inssa@gmail.com', 'inssa', '$2a$10$aitoVPlyQWr2glas8wj5CO7En.h19kob7DW0KPNDCQcydynEkpqMS', 'USER', 'ACTIVATED');
-INSERT INTO INSSA.user (created_date, modified_date, profile_no, quit_date, email, nickname, password, role, status) VALUES ('2023-08-08 23:43:10.244423', '2023-08-08 23:43:10.244423', null, null, 'inssa@naver.com', 'inssa-test', '$2a$10$arx.W4d17jykL5zvQGrgkOdQfgJ/O.e11HkcfAsIQrJuEKFwRQ8Cm', 'USER', 'ACTIVATED');
+INSERT INTO INSSA.user (created_date, modified_date, profile_no, quit_date, email, nickname, password, role, status) VALUES ('2023-08-08 23:43:10.244423', '2023-08-08 23:43:10.244423', null, null, 'inssa@naver.com', '나무', '$2a$10$09.e19cKkNDSqzlXM6DkquKq5hfZwcM87BdViS3giy/y9.aFqDAsq', 'USER', 'ACTIVATED');
 
-INSERT INTO inssa.company (REGISTRATION_NO, COMPANY_NAME, CONTACT_NUMBER, STATUS, APPROVAL, RATING, USER_NO) VALUES ('111', '회사test', '01011112222', 'Y', 'N', null, 1);
+INSERT INTO inssa.company (REGISTRATION_NO, COMPANY_NAME, CONTACT_NUMBER, STATUS, APPROVAL, RATING, USER_NO) VALUES ('111', '회사test', '01011112222', 'Y', 'N', null, 2);
 
 INSERT INTO inssa.build_review (view_count, company_no, created_date, modified_date, user_no, status, title, content) VALUES (0, 1, '2023-08-16 20:51:01.000000', '2023-08-16 20:51:03.000000', 2, 'VISIBLE', 'build title', 'build title');
 INSERT INTO inssa.build_review (view_count, company_no, created_date, modified_date, user_no, status, title, content) VALUES (0, 1, '2023-08-16 20:51:01.000000', '2023-08-16 20:51:03.000000', 1, 'VISIBLE', 'build title 2', 'build title 2');
