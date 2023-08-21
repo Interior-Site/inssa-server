0dff60f8b3a35de2bb1d0f734ab64ab16606852e
Dahee Goo <dadaahee@gmail.com>
2023-08-21 17:55:51 +0900

    chore: response example 추가, validation 일부 수정

--- src/main/java/com/inssa/server/api/review/order/OrderController.java
+++ src/main/java/com/inssa/server/api/review/order/OrderController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -36,8 +37,24 @@ public class OrderController {
     @Operation(summary = "견적 후기 등록 API", tags = "orderReview")
     @ApiResponses(value = {
             @ApiResponse(
-                    responseCode = "201",
-                    description = "Created"
+                    responseCode = "200",
+                    description = "등록 성공",
+                    content = @Content(
+                            examples = @ExampleObject(
+                                    name = "CREATED",
+                                    value = """
+                                            {
+                                                    "message":{
+                                                        "code":201,
+                                                        "message":"CREATED"
+                                                    },
+                                                    "result": {
+                                                        "orderReviewNo": 3
+                                                    }
+                                                }
+                                            """
+                            )
+                    )
             )
     })
     @PreAuthorizeLogInUser
@@ -89,10 +106,6 @@ public class OrderController {
                     )
             }
     )
-    @Parameter(in = ParameterIn.QUERY
-            , description = "조회할 견적 후기 번호"
-            , name = "orderReviewNo"
-            , content = @Content(schema = @Schema(type = "integer")))
     @GetMapping("/order/{orderReviewNo}")
     public InssaApiResponse<OrderReviewResponseDto> findById(
             @PathVariable Long orderReviewNo
@@ -104,8 +117,25 @@ public class OrderController {
     @Operation(summary = "견적 후기 수정 API", tags = "orderReview")
     @ApiResponses(value = {
             @ApiResponse(
-                    responseCode = "202",
-                    description = "Updated"
+                    responseCode = "200",
+                    description = "수정 성공",
+                    content = @Content(
+                            examples = @ExampleObject(
+                                    name = "UPDATED",
+                                    value = """
+                                            {
+                                                    "message":{
+                                                        "code":202,
+                                                        "message":"UPDATED"
+                                                    },
+                                                    "result": {
+                                                        "orderReviewNo": 3
+                                                    }
+                                                }
+                                            """
+                            )
+                    )
+
             )
     })
     @PreAuthorizeLogInUser
@@ -131,14 +161,26 @@ public class OrderController {
     @Operation(summary = "견적 후기 삭제 API", tags = "orderReview")
     @ApiResponses(value = {
             @ApiResponse(
-                    responseCode = "203",
-                    description = "Deleted"
+                    responseCode = "200",
+                    description = "삭제 성공",
+                    content = @Content(
+                            examples = @ExampleObject(
+                                    name = "DELETED",
+                                    value = """
+                                            {
+                                                    "message":{
+                                                        "code":203,
+                                                        "message":"DELETED"
+                                                    },
+                                                    "result": {
+                                                        "orderReviewNo": 3
+                                                    }
+                                                }
+                                            """
+                            )
+                    )
             )
     })
-    @Parameter(in = ParameterIn.QUERY
-            , description = "삭제할 견적 후기 번호"
-            , name = "orderReviewNo"
-            , content = @Content(schema = @Schema(type = "integer")))
     @PreAuthorizeLogInUser
     @DeleteMapping("/order/{orderReviewNo}")
     public InssaApiResponse<OrderReviewDeleteResponseDto> deleteOrderReview(
