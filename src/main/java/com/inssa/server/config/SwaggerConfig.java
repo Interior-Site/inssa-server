750f786c38c21749d917ddd0f310884ddf1912b0
Dahee Goo <dadaahee@gmail.com>
2023-08-21 17:55:24 +0900

    feat: swagger authorization 기능 추가

--- src/main/java/com/inssa/server/config/SwaggerConfig.java
+++ src/main/java/com/inssa/server/config/SwaggerConfig.java
@@ -1,21 +1,127 @@
 package com.inssa.server.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-		info = @Info(title = "Inssa API",
-				description = "Inssa API 명세서",
-				version = "v1"))
 @Configuration
 public class SwaggerConfig {
+
+	/**
+	 * Basic Swagger Description
+	 */
+	private static final String TITLE = "Inssa API";
+	private static final String DESCRIPTION = "Inssa API 명세서";
+	private static final String VERSION = "v1.0.0.";
+
+	private static final String AUTH_NAME = "jwtAuth";
+
+
+	private Info getInfo() {
+		return new Info()
+				.title(TITLE)
+				.description(DESCRIPTION)
+				.version(VERSION);
+	}
+
+	private SecurityScheme getSecurityScheme() {
+		return new SecurityScheme()
+				.name("Authorization")
+				.in(SecurityScheme.In.HEADER)
+				.type(SecurityScheme.Type.HTTP)
+				.scheme("bearer")
+				.bearerFormat("{{ Bearer }}");
+	}
+
+
 	@Bean
 	public OpenAPI openAPI() {
+		Info info = getInfo();
+
+		// API 요청헤더에 인증정보 포함
+		Components components = new Components()
+				.addSecuritySchemes(AUTH_NAME, getSecurityScheme());
+		SecurityRequirement securityItem = new SecurityRequirement().addList(AUTH_NAME);
+
 		return new OpenAPI()
-				.components(new Components());
+				.info(info)
+				.components(components)
+				.addSecurityItem(securityItem);
 	}
+
+
+	/**
+	 * API Groups
+	 */
+
+	@Bean
+	public GroupedOpenApi allGroup() {
+		return GroupedOpenApi.builder()
+				.group("All - 전체 보기")
+				.packagesToScan("com.inssa.server.api")
+				.build();
+	}
+
+	@Bean
+	public GroupedOpenApi boardGroup() {
+		return GroupedOpenApi.builder()
+				.group("Board - 게시판")
+			 	.packagesToScan("com.inssa.server.api.board")
+				.build();
+	}
+
+	@Bean
+	public GroupedOpenApi bookmarkGroup() {
+		return GroupedOpenApi.builder()
+				.group("Bookmark - 찜")
+				.packagesToScan("com.inssa.server.api.bookmark")
+				.build();
+	}
+
+	@Bean
+	public GroupedOpenApi commentGroup() {
+		return GroupedOpenApi.builder()
+				.group("Comment - 댓글")
+				.packagesToScan("com.inssa.server.api.comment")
+				.build();
+	}
+
+	@Bean
+	public GroupedOpenApi companyGroup() {
+		return GroupedOpenApi.builder()
+				.group("Company - 기업")
+				.packagesToScan("com.inssa.server.api.company")
+				.build();
+	}
+
+	@Bean
+	public GroupedOpenApi imageGroup() {
+		return GroupedOpenApi.builder()
+				.group("Image - 이미지")
+				.packagesToScan("com.inssa.server.api.image")
+				.build();
+	}
+
+	@Bean
+	public GroupedOpenApi reviewGroup() {
+		return GroupedOpenApi.builder()
+				.group("Review - 후기")
+				.packagesToScan("com.inssa.server.api.review")
+				.build();
+	}
+
+	@Bean
+	public GroupedOpenApi userGroup() {
+		return GroupedOpenApi.builder()
+				.group("User - 사용자")
+				.packagesToScan("com.inssa.server.api.user")
+				.build();
+	}
+
+
 }
