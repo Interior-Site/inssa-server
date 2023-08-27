ce6744c3b556a12d6f921d7a2729f8492fc077ee
Dahee Goo <dadaahee@gmail.com>
2023-08-28 00:22:21 +0900

    chore: post 임시 클래스 추가

---
+++ src/main/java/com/inssa/server/api/board/post/model/Post.java
@@ -0,0 +1,15 @@
+package com.inssa.server.api.board.post.model;
+
+import com.inssa.server.share.board.BoardStatus;
+import com.inssa.server.share.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+
+@Entity
+public class Post extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long no;
+
+    @Enumerated(EnumType.STRING)
+    private BoardStatus status;
+}
