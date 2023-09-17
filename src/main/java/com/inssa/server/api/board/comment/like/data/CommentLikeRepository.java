76dd81aa493579b7e1c7e2ba79b27eb4ada62323
Dahee Goo <dadaahee@gmail.com>
2023-09-17 12:35:08 +0900

    refactor: 댓글 공감 API 리팩토링

--- src/main/java/com/inssa/server/api/board/comment/like/data/CommentLikeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.inssa.server.api.board.comment.like.data;
-
-import com.inssa.server.api.board.comment.like.model.CommentLike;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
-    boolean existsByUserNoAndCommentNo(Long userNo, Long commentNo);
-}
---
+++ src/main/java/com/inssa/server/api/board/commentlike/model/CommentLike.java
@@ -0,0 +1,38 @@
+package com.inssa.server.api.board.commentlike.model;
+
+import com.inssa.server.api.board.comment.model.Comment;
+import com.inssa.server.api.user.model.User;
+import com.inssa.server.share.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class CommentLike extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_no")
+    private Long no;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "userNo", insertable = false, updatable = false)
+    private User user;
+    private Long userNo;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "commentNo", insertable = false, updatable = false)
+    private Comment comment;
+    private Long commentNo;
+
+    @Builder
+    public CommentLike(Long userNo, Long commentNo) {
+        this.userNo = userNo;
+        this.commentNo = commentNo;
+    }
+
+    public void changeComment(Comment comment) {
+        this.comment = comment;
+    }
+}
