c54daa403410b70f572ee130e38d4e0756d7d1bf
Aram <df0404@hanmail.net>
2023-10-15 15:49:57 +0900

    fix : review 패키지 삭제 및 Category, BuildType Enum 생성

---
+++ src/main/java/com/inssa/server/api/board/post/model/BuildType.java
@@ -0,0 +1,8 @@
+package com.inssa.server.api.board.post.model;
+
+/**
+ * 건물 유형: 아파트, 빌라, 오피스텔, 원룸/투룸, 카페, 식당, 기타
+ */
+public enum BuildType {
+    APARTMENT, VILLA, OFFICETEL, ONE_ROOM, TWO_ROOM, CAFE, RESTAURANT, ETC
+}
---
+++ src/main/java/com/inssa/server/api/board/post/model/Category.java
@@ -0,0 +1,7 @@
+package com.inssa.server.api.board.post.model;
+/**
+ * 시공 유형: 벽지, 도배, 장판, 샷시, 줄눈, 확장, 타일
+ */
+public enum Category {
+    WALLPAPER, PAPERING, FLOORING, SASH, GROUT, EXPANSION, TILE
+}
